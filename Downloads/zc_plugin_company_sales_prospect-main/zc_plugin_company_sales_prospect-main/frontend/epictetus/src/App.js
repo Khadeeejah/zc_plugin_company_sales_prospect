333503e897e63b6efd855cc2401212b19156db83
Khadijah Amusat <wuraola81@gmail.com>
2021-09-01 12:26:54 +0200

    added test.js

---
+++ Downloads/zc_plugin_company_sales_prospect-main/zc_plugin_company_sales_prospect-main/frontend/epictetus/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="text-4xl font-bold">Company Sales Prospect</h1>
+    </div>
+  );
+}
+
+export default App;
