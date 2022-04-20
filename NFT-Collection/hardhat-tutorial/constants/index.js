1a817f7193e918105afffb05c378cacc41030c6c
Khadeeejah <wuraola81@gmail.com>
2022-04-20 14:17:47 +0200

    new

---
+++ NFT-Collection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x15c8f0F1eB62D177C6db0d6D08558cBF62D9856d";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-snowy-zeta.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
